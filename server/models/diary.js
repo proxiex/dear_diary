9d2573bd977a075276e4806f5b4aa47cd5998bed
proxiex <difrentecho@gmail.com>
2018-10-08 03:03:43 +0100

    add entry feature - authenticate user - validate entry

--- server/models/diary.js
+++ server/models/diary.js
@@ -1,11 +1,22 @@
 import mongoose from 'mongoose';
 
+const sortAnswers = (a, b) => {
+  if (a.createdAt === b.createdAt) {
+    return b.updatedAt - a.updatedAt;
+  }
+  return b.createdAt - a.createdAt;
+};
+
+
 const { Schema } = mongoose;
 const diarySchema = Schema({
   user_id: { type: Schema.Types.ObjectId, ref: 'User' },
   title: String,
   text: String,
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  private: {
+    type: Boolean,
+    default: true
+  },
   createdAt: {
     type: Date,
     default: Date.now
@@ -19,6 +30,11 @@ const diarySchema = Schema({
 diarySchema.method('update', (updates, callback) => {
   Object.assign(this, updates, { updatedAt: new Date() });
   this.save(callback);
+});
+
+diarySchema.pre('save', (next) => {
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 const Diary = mongoose.model('Diary', diarySchema);
